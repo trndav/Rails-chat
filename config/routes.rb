b429b44cae054f59bb5b7e3f8954c335bbd73656
trndav <farsiteguide@gmail.com>
2023-08-17 22:00:26 +0200

    "Added message posting from UI"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   root "chatroom#index"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   get "logout", to: "sessions#destroy"
+  post "message", to: "messages#create"
 end
