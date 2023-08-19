af3c49b0d64b981493975599da76378b9bbef0b9
trndav <farsiteguide@gmail.com>
2023-08-19 10:43:58 +0200

    "Last 14 messages appear on chat"

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
     belongs_to :user
     validates :body, presence: true
-    scope :custom_display, -> { order(:created_at).last(10) }
+    scope :custom_display, -> { order(:created_at).last(14) }
 end
